c2c4ee0d8dc9ec1203a76a1151cddc0c9f8a56f9
sPridmore96 <93345198+sPridmore96@users.noreply.github.com>
2022-08-21 13:15:04 +0100

    styling changes

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import './Home.scss';
-import Map from '../../components/Map/Map';
-import WeatherSnippet from '../WeatherSnippet/WeatherSnippet';
-import NavBar from '../../components/NavBar/NavBar';
+import React, { useState } from "react";
+import "./Home.scss";
+import Map from "../../components/Map/Map";
+import WeatherSnippet from "../WeatherSnippet/WeatherSnippet";
+import NavBar from "../../components/NavBar/NavBar";
+import StaticMap from "../../components/StaticMap/StaticMap";
 
 const Home = ({
   longitude,
@@ -10,24 +11,29 @@ const Home = ({
   currentWeatherInfo,
   getIcon,
   localForecast,
+  userName,
+  userWorkPlace,
 }) => {
   return (
     <div className="home">
-      <h1 className="home__title">Hey Name</h1>
+      <h1 className="home__title">Hey {userName}</h1>
       <p>Here is some off todays info!</p>
       <h2 className="home__subheader">You're Commute</h2>
-      <Map
-        currentWeatherInfo={currentWeatherInfo}
-        latitude={latitude}
-        longitude={longitude}
-      />
+      <div className="home__map">
+        <StaticMap
+          currentWeatherInfo={currentWeatherInfo}
+          latitude={latitude}
+          longitude={longitude}
+          userWorkPlace={userWorkPlace}
+        />
+      </div>
       <h2 className="home__subheader">Your local Forecast</h2>
 
       <div className="home__weather">
         <WeatherSnippet
           localForecast={localForecast}
           getIcon={getIcon}
-          id={localForecast.list.forEach((item) => {})}
+          id={localForecast.list.forEach((item) => item.id)}
         />
       </div>
       <NavBar />
